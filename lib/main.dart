d0adc2070e1d729931b923406b9e20ff59d48532
B07207012 <b07207012@ntu.edu.tw>
2023-11-19 14:36:37 +0800

    feat: move fraud line id page from origin project

--- lib/main.dart
+++ lib/main.dart
@@ -14,20 +14,81 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const Homepage());
+        home: const FraudIDPage());
   }
 }
 
-class Homepage extends StatefulWidget {
-  const Homepage({super.key});
+class FraudIDPage extends StatefulWidget {
+  const FraudIDPage({super.key});
 
   @override
-  State<Homepage> createState() => _HomepageState();
+  State<FraudIDPage> createState() => _FraudIDPageState();
 }
 
-class _HomepageState extends State<Homepage> {
+class _FraudIDPageState extends State<FraudIDPage> {
+  late TextEditingController _textEditingController;
+
+  @override
+  void initState() {
+    _textEditingController = TextEditingController();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        SearchField(
+          hintText: "有疑慮的 Line ID",
+          controller: _textEditingController,
+          onChanged: (value) {},
+        ),
+        const FraudLineIDSection(),
+      ],
+    );
+  }
+}
+
+class FraudLineIDSection extends StatelessWidget {
+  const FraudLineIDSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(children: []),
+      ),
+    );
+  }
+}
+
+class SearchField extends StatelessWidget {
+  final String hintText;
+  final TextEditingController controller;
+  final Function onChanged;
+  const SearchField(
+      {super.key,
+      this.hintText = "",
+      required this.controller,
+      required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 30),
+      padding: const EdgeInsets.all(10),
+      child: TextField(
+        decoration: InputDecoration(
+            labelText: "Search",
+            hintText: hintText,
+            hintStyle: TextStyle(color: Colors.grey.shade400),
+            prefixIcon: const Icon(Icons.search),
+            border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)))),
+        controller: controller,
+        onChanged: (value) => onChanged(value),
+      ),
+    );
   }
 }
